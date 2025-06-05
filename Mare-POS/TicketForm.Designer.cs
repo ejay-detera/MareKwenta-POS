85442bddd7f7a464d93f14107dc09b46f73123c2
liberThree <libertykeithca@gmail.com>
2025-06-05 13:51:29 +0800

    Ticket Form (Unfinished)

---
+++ Mare-POS/TicketForm.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace Mare_POS
+{
+    partial class TicketForm
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TicketForm));
+            panel2 = new Panel();
+            label1 = new Label();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            panel1 = new Panel();
+            cuiPanel1 = new CuoreUI.Controls.cuiPanel();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            cuiPanel2 = new CuoreUI.Controls.cuiPanel();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            cuiPanel3 = new CuoreUI.Controls.cuiPanel();
+            label3 = new Label();
+            pictureBox2 = new PictureBox();
+            cuiPanel4 = new CuoreUI.Controls.cuiPanel();
+            label4 = new Label();
+            pictureBox3 = new PictureBox();
+            cuiPanel5 = new CuoreUI.Controls.cuiPanel();
+            label5 = new Label();
+            pictureBox4 = new PictureBox();
+            panel1.SuspendLayout();
+            cuiPanel1.SuspendLayout();
+            flowLayoutPanel2.SuspendLayout();
+            cuiPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            cuiPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            cuiPanel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            cuiPanel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.Dock = DockStyle.Right;
+            panel2.Location = new Point(808, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(496, 901);
+            panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Unbounded Medium", 28.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(78, 45, 24);
+            label1.Location = new Point(5, 12);
+            label1.Name = "label1";
+            label1.Padding = new Padding(20, 0, 0, 0);
+            label1.Size = new Size(364, 74);
+            label1.TabIndex = 0;
+            label1.Text = "Hello, User";
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Location = new Point(77, 89);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(669, 59);
+            flowLayoutPanel1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(cuiPanel5);
+            panel1.Controls.Add(cuiPanel4);
+            panel1.Controls.Add(cuiPanel3);
+            panel1.Controls.Add(flowLayoutPanel2);
+            panel1.Controls.Add(cuiPanel1);
+            panel1.Controls.Add(flowLayoutPanel1);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(802, 901);
+            panel1.TabIndex = 0;
+            // 
+            // cuiPanel1
+            // 
+            cuiPanel1.AutoSize = true;
+            cuiPanel1.Controls.Add(button4);
+            cuiPanel1.Controls.Add(button3);
+            cuiPanel1.Controls.Add(button2);
+            cuiPanel1.Controls.Add(button1);
+            cuiPanel1.Location = new Point(80, 89);
+            cuiPanel1.Name = "cuiPanel1";
+            cuiPanel1.OutlineThickness = 1F;
+            cuiPanel1.PanelColor = Color.FromArgb(242, 239, 234);
+            cuiPanel1.PanelOutlineColor = Color.Silver;
+            cuiPanel1.Rounding = new Padding(20);
+            cuiPanel1.Size = new Size(642, 56);
+            cuiPanel1.TabIndex = 0;
+            cuiPanel1.Paint += cuiPanel1_Paint;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Transparent;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Unbounded", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.ForeColor = Color.FromArgb(78, 45, 24);
+            button4.Location = new Point(476, 8);
+            button4.Name = "button4";
+            button4.Size = new Size(110, 40);
+            button4.TabIndex = 3;
+            button4.Text = "Food";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Transparent;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Unbounded", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.FromArgb(78, 45, 24);
+            button3.Location = new Point(273, 8);
+            button3.Name = "button3";
+            button3.Size = new Size(187, 40);
+            button3.TabIndex = 2;
+            button3.Text = "Non-Coffee";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Unbounded", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.FromArgb(78, 45, 24);
+            button2.Location = new Point(131, 9);
+            button2.Name = "button2";
+            button2.Size = new Size(128, 40);
+            button2.TabIndex = 1;
+            button2.Text = "Coffee";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Unbounded", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.FromArgb(78, 45, 24);
+            button1.Location = new Point(17, 9);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 40);
+            button1.TabIndex = 0;
+            button1.Text = "All";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.Controls.Add(cuiPanel2);
+            flowLayoutPanel2.Location = new Point(26, 175);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(748, 215);
+            flowLayoutPanel2.TabIndex = 2;
+            // 
+            // cuiPanel2
+            // 
+            cuiPanel2.BackColor = Color.Transparent;
+            cuiPanel2.Controls.Add(label2);
+            cuiPanel2.Controls.Add(pictureBox1);
+            cuiPanel2.Location = new Point(3, 3);
+            cuiPanel2.Margin = new Padding(3, 3, 12, 3);
+            cuiPanel2.Name = "cuiPanel2";
+            cuiPanel2.OutlineThickness = 3F;
+            cuiPanel2.PanelColor = Color.White;
+            cuiPanel2.PanelOutlineColor = Color.White;
+            cuiPanel2.Rounding = new Padding(25);
+            cuiPanel2.Size = new Size(176, 215);
+            cuiPanel2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(13, 13);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(147, 147);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Unbounded Medium", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(78, 45, 24);
+            label2.Location = new Point(13, 163);
+            label2.Name = "label2";
+            label2.Size = new Size(147, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Americano";
+            // 
+            // cuiPanel3
+            // 
+            cuiPanel3.BackColor = Color.Transparent;
+            cuiPanel3.Controls.Add(label3);
+            cuiPanel3.Controls.Add(pictureBox2);
+            cuiPanel3.Location = new Point(220, 175);
+            cuiPanel3.Margin = new Padding(3, 3, 10, 3);
+            cuiPanel3.Name = "cuiPanel3";
+            cuiPanel3.OutlineThickness = 3F;
+            cuiPanel3.PanelColor = Color.White;
+            cuiPanel3.PanelOutlineColor = Color.White;
+            cuiPanel3.Rounding = new Padding(25);
+            cuiPanel3.Size = new Size(176, 215);
+            cuiPanel3.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Unbounded Medium", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(78, 45, 24);
+            label3.Location = new Point(13, 163);
+            label3.Name = "label3";
+            label3.Size = new Size(147, 25);
+            label3.TabIndex = 1;
+            label3.Text = "Americano";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(13, 13);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(147, 147);
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // cuiPanel4
+            // 
+            cuiPanel4.BackColor = Color.Transparent;
+            cuiPanel4.Controls.Add(label4);
+            cuiPanel4.Controls.Add(pictureBox3);
+            cuiPanel4.Location = new Point(409, 175);
+            cuiPanel4.Margin = new Padding(3, 3, 10, 3);
+            cuiPanel4.Name = "cuiPanel4";
+            cuiPanel4.OutlineThickness = 3F;
+            cuiPanel4.PanelColor = Color.White;
+            cuiPanel4.PanelOutlineColor = Color.White;
+            cuiPanel4.Rounding = new Padding(25);
+            cuiPanel4.Size = new Size(176, 215);
+            cuiPanel4.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.Font = new Font("Unbounded Medium", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.FromArgb(78, 45, 24);
+            label4.Location = new Point(13, 163);
+            label4.Name = "label4";
+            label4.Size = new Size(147, 25);
+            label4.TabIndex = 1;
+            label4.Text = "Americano";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(13, 13);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(147, 147);
+            pictureBox3.TabIndex = 0;
+            pictureBox3.TabStop = false;
+            // 
+            // cuiPanel5
+            // 
+            cuiPanel5.BackColor = Color.Transparent;
+            cuiPanel5.Controls.Add(label5);
+            cuiPanel5.Controls.Add(pictureBox4);
+            cuiPanel5.Location = new Point(598, 175);
+            cuiPanel5.Margin = new Padding(3, 3, 10, 3);
+            cuiPanel5.Name = "cuiPanel5";
+            cuiPanel5.OutlineThickness = 3F;
+            cuiPanel5.PanelColor = Color.White;
+            cuiPanel5.PanelOutlineColor = Color.White;
+            cuiPanel5.Rounding = new Padding(25);
+            cuiPanel5.Size = new Size(176, 215);
+            cuiPanel5.TabIndex = 3;
+            // 
+            // label5
+            // 
+            label5.Font = new Font("Unbounded Medium", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.FromArgb(78, 45, 24);
+            label5.Location = new Point(13, 163);
+            label5.Name = "label5";
+            label5.Size = new Size(147, 25);
+            label5.TabIndex = 1;
+            label5.Text = "Americano";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(13, 13);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(147, 147);
+            pictureBox4.TabIndex = 0;
+            pictureBox4.TabStop = false;
+            // 
+            // TicketForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(242, 239, 234);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "TicketForm";
+            Size = new Size(1304, 901);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            cuiPanel1.ResumeLayout(false);
+            flowLayoutPanel2.ResumeLayout(false);
+            cuiPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            cuiPanel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            cuiPanel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            cuiPanel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Panel panel2;
+        private Label label1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private CuoreUI.Controls.cuiPanel cuiPanel1;
+        private Panel panel1;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private CuoreUI.Controls.cuiPanel cuiPanel2;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private CuoreUI.Controls.cuiPanel cuiPanel5;
+        private Label label5;
+        private PictureBox pictureBox4;
+        private CuoreUI.Controls.cuiPanel cuiPanel4;
+        private Label label4;
+        private PictureBox pictureBox3;
+        private CuoreUI.Controls.cuiPanel cuiPanel3;
+        private Label label3;
+        private PictureBox pictureBox2;
+    }
+}
