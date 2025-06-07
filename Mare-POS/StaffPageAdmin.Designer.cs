7a8762a6cfa5bf070dc3dc11d963fd6b39d0b971
ayayeyyyyy <aliahsdelrosario@iskolarngbayan.pup.edu.ph>
2025-06-07 23:22:45 +0800

    staffpage admin done

--- Mare-POS/StaffPageAdmin.Designer.cs
+++ Mare-POS/StaffPageAdmin.Designer.cs
@@ -33,7 +33,9 @@ namespace Mare_POS
             cuiLabel1 = new cuiLabel();
             dateHeader = new cuiLabel();
             dataGridView1 = new DataGridView();
+            panelRoundedContainer = new Panel();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panelRoundedContainer.SuspendLayout();
             SuspendLayout();
             // 
             // cuiLabel1
@@ -43,10 +45,10 @@ namespace Mare_POS
             cuiLabel1.Font = new Font("Unbounded Medium", 27F, FontStyle.Bold);
             cuiLabel1.ForeColor = Color.FromArgb(78, 45, 24);
             cuiLabel1.HorizontalAlignment = StringAlignment.Near;
-            cuiLabel1.Location = new Point(31, 27);
-            cuiLabel1.Margin = new Padding(5);
+            cuiLabel1.Location = new Point(35, 36);
+            cuiLabel1.Margin = new Padding(6, 7, 6, 7);
             cuiLabel1.Name = "cuiLabel1";
-            cuiLabel1.Size = new Size(182, 48);
+            cuiLabel1.Size = new Size(208, 64);
             cuiLabel1.TabIndex = 1;
             cuiLabel1.VerticalAlignment = StringAlignment.Center;
             cuiLabel1.Load += cuiLabel1_Load;
@@ -54,35 +56,55 @@ namespace Mare_POS
             // dateHeader
             // 
             dateHeader.Content = "";
-            dateHeader.Font = new Font("Unbounded", 17.9999981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dateHeader.Font = new Font("Unbounded", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             dateHeader.ForeColor = Color.FromArgb(78, 45, 24);
             dateHeader.HorizontalAlignment = StringAlignment.Far;
-            dateHeader.Location = new Point(932, 30);
-            dateHeader.Margin = new Padding(4, 5, 4, 5);
+            dateHeader.Location = new Point(1056, 40);
+            dateHeader.Margin = new Padding(5, 7, 5, 7);
             dateHeader.Name = "dateHeader";
-            dateHeader.Size = new Size(350, 45);
+            dateHeader.Size = new Size(400, 60);
             dateHeader.TabIndex = 5;
-            dateHeader.VerticalAlignment = StringAlignment.Far;
+            dateHeader.VerticalAlignment = StringAlignment.Center;
+            dateHeader.Load += dateHeader_Load;
             // 
             // dataGridView1
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(31, 83);
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.SunkenHorizontal;
+            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridView1.ColumnHeadersHeight = 50;
+            dataGridView1.Location = new Point(19, 19);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(1213, 485);
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1384, 606);
             dataGridView1.TabIndex = 6;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // panelRoundedContainer
+            // 
+            panelRoundedContainer.BackColor = Color.White;
+            panelRoundedContainer.Controls.Add(dataGridView1);
+            panelRoundedContainer.Location = new Point(35, 110);
+            panelRoundedContainer.Name = "panelRoundedContainer";
+            panelRoundedContainer.Size = new Size(1421, 1167);
+            panelRoundedContainer.TabIndex = 7;
             // 
             // StaffPageAdmin
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(242, 239, 234);
-            Controls.Add(dataGridView1);
+            Controls.Add(panelRoundedContainer);
             Controls.Add(cuiLabel1);
             Controls.Add(dateHeader);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "StaffPageAdmin";
-            Size = new Size(1304, 985);
+            Size = new Size(1490, 1313);
+            Load += StaffPageAdmin_Load_1;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panelRoundedContainer.ResumeLayout(false);
             ResumeLayout(false);
         }
         private void StyleCafeGrid(DataGridView dgv)
@@ -93,7 +115,7 @@ namespace Mare_POS
             dgv.EnableHeadersVisualStyles = false;
             dgv.RowHeadersVisible = false;
             dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dgv.RowTemplate.Height = 40;
+            dgv.RowTemplate.Height = 50;
             dgv.AllowUserToAddRows = false;
 
             // Colors
@@ -103,21 +125,21 @@ namespace Mare_POS
             Color orangeAccent = Color.FromArgb(255, 140, 0);
 
             // Header style
-            dgv.ColumnHeadersDefaultCellStyle.BackColor = coffee;
-            dgv.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
-            dgv.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI Semibold", 11, FontStyle.Bold);
+            dgv.ColumnHeadersDefaultCellStyle.BackColor = Color.White;
+            dgv.ColumnHeadersDefaultCellStyle.ForeColor = coffee;
+            dgv.ColumnHeadersDefaultCellStyle.Font = new Font("Inter", 11, FontStyle.Bold);
             dgv.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
 
             // Row style
-            dgv.DefaultCellStyle.BackColor = cream;
+            dgv.DefaultCellStyle.BackColor = Color.White;
             dgv.DefaultCellStyle.ForeColor = Color.Black;
-            dgv.DefaultCellStyle.Font = new Font("Segoe UI", 10);
-            dgv.DefaultCellStyle.SelectionBackColor = orangeAccent;
-            dgv.DefaultCellStyle.SelectionForeColor = Color.White;
+            dgv.DefaultCellStyle.Font = new Font("Inter", 10);
+            //dgv.DefaultCellStyle.SelectionBackColor = orangeAccent;
+            //dgv.DefaultCellStyle.SelectionForeColor = Color.White;
             dgv.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
 
             // Alternating rows
-            dgv.AlternatingRowsDefaultCellStyle.BackColor = tan;
+            //dgv.AlternatingRowsDefaultCellStyle.BackColor = tan;
 
             // Grid color
             dgv.GridColor = tan;
@@ -128,6 +150,7 @@ namespace Mare_POS
         private CuoreUI.Controls.cuiLabel cuiLabel1;
         private CuoreUI.Controls.cuiLabel dateHeader;
         private DataGridView dataGridView1;
+        private Panel panelRoundedContainer;
     }
 }
 
--- Mare-POS/StaffPageAdmin.cs
+++ Mare-POS/StaffPageAdmin.cs
@@ -14,7 +14,7 @@ namespace Mare_POS
 {
     public partial class StaffPageAdmin : UserControl
     {
-        
+
         public StaffPageAdmin()
         {
             InitializeComponent();
@@ -25,7 +25,15 @@ namespace Mare_POS
         {
             dateHeader.Content = DateTime.Now.ToString("MMMM dd, yyyy");
             getData();
+
+            if (dataGridView1.Columns.Contains("Password"))
+                dataGridView1.Columns["Password"].Visible = false;
+            if (dataGridView1.Columns.Contains("Id"))
+                dataGridView1.Columns["Id"].Visible = false;
+
+            MakeRoundedPanel(panelRoundedContainer, 30);
             StyleCafeGrid(dataGridView1);
+            //dataGridView1.ClearSelection();
         }
         private void cuiLabel1_Load(object sender, EventArgs e)
         {
@@ -49,9 +57,36 @@ namespace Mare_POS
             DataTable dataTable = new DataTable();
             dataTable.Load(reader);
             dataGridView1.DataSource = dataTable;
-            
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                dataGridView1.ClearSelection();
+            });
         }
 
+        private void StaffPageAdmin_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateHeader_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void MakeRoundedPanel(Panel panel, int radius)
+        {
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            path.AddArc(0, 0, radius, radius, 180, 90); // Top left corner
+            path.AddArc(panel.Width - radius, 0, radius, radius, 270, 90); // Top right
+            path.AddArc(panel.Width - radius, panel.Height - radius, radius, radius, 0, 90); // Bottom right
+            path.AddArc(0, panel.Height - radius, radius, radius, 90, 90); // Bottom left
+            path.CloseAllFigures();
+            panel.Region = new Region(path);
+        }
     }
 }
     
